4a00cd94fdae927d7774c43f0ea28f51dc921c93
ba1durr <82411667+ba1durr@users.noreply.github.com>
2021-11-13 01:30:06 +0400

    connected the website to mySQL database

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+//connect to database
+
+$conn = mysqli_connect('localhost', 'Luke', 'Lukito123.', 'ninja_pizza');
+
+//check connection
+
+if(!$conn){
+    echo 'Connection error: ' . mysqli_connect_error();
+}
+
+
+
+
+?>
+
+
+
 
 
 <!DOCTYPE html>
