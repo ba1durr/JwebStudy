b36a4395cfc89cbf926ab555d775c3fed8ec573b
ba1durr <82411667+ba1durr@users.noreply.github.com>
2021-11-14 18:16:50 +0400

    learned alternate syntax for functions and added counter for the products

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
 
 //connect to database
-
 $conn = mysqli_connect('localhost', 'Luke', 'Lukito123.', 'ninja_pizza');
 
 //check connection
-
 if(!$conn){
     echo 'Connection error: ' . mysqli_connect_error();
 }
@@ -28,7 +26,6 @@ if(!$conn){
 
     //explode(',' , $pizzas[0]['ingredients']);
 
-
 ?>
 
 <!DOCTYPE html>
@@ -40,26 +37,39 @@ if(!$conn){
     <div class="container">
         <div class="row">
 
-            <?php foreach($pizzas as $pizza){?>
+            <?php foreach($pizzas as $pizza): ?>
 
                 <div class="col s6 md3">
                     <div class="card z-depth-0">
                         <div class="card-content center">
                             <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-                            <ul>
-                                <?php foreach(explode(',' , $pizza['ingredients'])as $ing){; ?> 
-                                    <li><?php echo htmlspecialchars($ing); ?></li>    
-                                <?php } ?>    
-                            </ul>
+                                <ul>
+                                    <?php foreach(explode(',' , $pizza['ingredients'])as $ing): ?> 
+                                        <li><?php echo htmlspecialchars($ing); ?></li>    
+                                    <?php endforeach; ?>    
+                                </ul>
                         </div>
-                        <div class="card-action right-align">
+                            <div class="card-action right-align">
                             <a class="brand-text" href="#">more info</a>
                         </div>
                     </div>
                 </div>
+            <?php endforeach; ?>
 
+            <?php if(count($pizzas)>=2): ?>
+                <p>there are 2 or more pizzas</p>
+            <?php else:?>
+                <p>there are less than 2 pizzas</p>
+            <?php endif; ?>
+
+            
+            <?php if(count($pizzas)>=2) { ?>
+                <p>there are 2 or more pizzas</p>
+            <?php } else {?>
+                <p>there are less than 2 pizzas</p>
             <?php } ?>
 
+            
         </div>
     </div>
 
