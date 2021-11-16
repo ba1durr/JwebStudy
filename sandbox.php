17abe6fc6bcfa00bf02f7a24e4f407fd0b365493
ba1durr <82411667+ba1durr@users.noreply.github.com>
2021-11-17 00:17:44 +0400

    Learned Superglobals and sessions

---
+++ sandbox.php
@@ -0,0 +1,54 @@
+<?php 
+
+//ternary operations
+// $score = 20;
+
+// if($score > 40){
+//     echo 'high score';
+// } else {
+//     echo 'low score :(';
+// }
+
+//echo $score > 40 ? 'High Score!' : 'Low Score :(';
+//<p><?php echo $score > 40 ? 'High Score!' : 'Low Score :('; 
+
+//SUPER GLOBALS
+// echo $_SERVER['SERVER_NAME'] . '<br />';
+// echo $_SERVER['REQUEST_METHOD'] . '<br />';
+// echo $_SERVER['SCRIPT_FILENAME'] . '<br />';
+// echo $_SERVER['PHP_SELF'] . '<br />';
+
+
+// sessions
+// $_SESSION, $_COOKIE
+
+
+if(isset($_POST['submit'])){
+
+    //when the user hits the submit button, session starts
+    session_start();
+
+    $_SESSION['name'] = $_POST['name'];
+
+    header('Location: index.php');
+
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP TUTS</title>
+</head>
+<body>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+    <input type="" name="name">
+    <input type="submit" name="submit" value="submit">
+</form>
+
+</body>
+</html>
