8c95415b810cadf613b822eae5756f8f1c8e2368
ba1durr <82411667+ba1durr@users.noreply.github.com>
2021-11-20 16:03:09 +0400

    made it simple to create new Users with a different method

--- study/test3.php
+++ study/test3.php
@@ -1,9 +1,13 @@
 <?php 
 
 class User{
-    //properties and methods
     public $username = 'baldur';
     public $email = 'baldur@odin.com';
+
+    public function __construct($username, $email){
+        $this->username = $username;
+        $this->email = $email;
+    }
 
     public function addFriend(){
         return "$this->username added a new friend";
@@ -11,25 +15,19 @@ class User{
 
 }
 
-$userOne = new User();
-$userTwo = new User();
+$userOne = new User('Mario','Mario@odin.com');
+$userTwo = new User('Freya','Freya@vanir.com');
 
 echo $userOne->username . '<br />';
 echo $userOne->email . '<br />';
 echo $userOne->addFriend() . '<br />';
 
-// print_r ( get_class_vars('User') );
-// print_r ( get_class_methods('User') );
-
-$userTwo->username = 'mimir';
-$userTwo->email = 'mimir@odin.com';
-
-
 echo $userTwo->username . '<br />';
 echo $userTwo->email . '<br />';
 echo $userTwo->addFriend() . '<br />';
 
-
+// print_r ( get_class_vars('User') );
+// print_r ( get_class_methods('User') );
 
 ?>
 
