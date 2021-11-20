38c496bf2b232664ada0b5b8a175925860a4a9ad
ba1durr <82411667+ba1durr@users.noreply.github.com>
2021-11-20 15:14:31 +0400

    delving deeper into OOP tutorials, getclass function

--- study/test2.php
+++ study/test2.php
@@ -2,38 +2,38 @@
 
 //classes
 
-class User {
+// class User {
 
-    private $email;
-    private $name;
+//     private $email;
+//     private $name;
 
-    public function __construct($name, $email)
-    {
-        // $this->email = 'a@b.com';
-        // $this->name = 'Luke';
-        $this->email = $email;
-        $this->name = $name;
-    }
+//     public function __construct($name, $email)
+//     {
+//         // $this->email = 'a@b.com';
+//         // $this->name = 'Luke';
+//         $this->email = $email;
+//         $this->name = $name;
+//     }
 
-    public function login(){
-        // echo 'the user logged in';
-        echo $this->name . ' has logged in';
-    }
+//     public function login(){
+//         // echo 'the user logged in';
+//         echo $this->name . ' has logged in';
+//     }
 
-    public function getName(){
-        return $this->name;
-    }
+//     public function getName(){
+//         return $this->name;
+//     }
 
-    public function setName($name){
-        if(is_string($name) && strlen($name)>1){
-            $this->name = $name;
-            return "name has been updated to $name";
-        } else{
-            return 'not a valid name';
-        }
-    }
+//     public function setName($name){
+//         if(is_string($name) && strlen($name)>1){
+//             $this->name = $name;
+//             return "name has been updated to $name";
+//         } else{
+//             return 'not a valid name';
+//         }
+//     }
 
-}
+// }
 
 
 // $userOne = new User();
@@ -41,11 +41,11 @@ class User {
 // $userOne->login();
 // echo $userOne->email;
 
-$userTwo = new User('Alex', 'Alex@b.com');
+// $userTwo = new User('Alex', 'Alex@b.com');
 
+// // echo $userTwo->getName();
+// // echo $userTwo->setName(50);
+// echo $userTwo->setName('Luke');
 // echo $userTwo->getName();
-// echo $userTwo->setName(50);
-echo $userTwo->setName('Luke');
-echo $userTwo->getName();
 
 ?>
---
+++ study/test3.php
@@ -0,0 +1,26 @@
+<?php 
+
+class User{
+
+    //properties and methods
+
+
+}
+
+$userOne = new User();
+$userTwo = new User();
+
+echo 'class of $userOne is ' . get_class($userOne);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>PHP OOP PRACTICE</title>
+</head>
+<body>
+ 
+</body>
+</html>
