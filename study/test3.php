a31715a1279012f5e5220a7d7c615c6c3c5c24e3
ba1durr <82411667+ba1durr@users.noreply.github.com>
2021-11-20 15:56:43 +0400

    redid previous test materials

--- study/test3.php
+++ study/test3.php
@@ -1,16 +1,35 @@
 <?php 
 
 class User{
-
     //properties and methods
+    public $username = 'baldur';
+    public $email = 'baldur@odin.com';
 
+    public function addFriend(){
+        return "$this->username added a new friend";
+    }
 
 }
 
 $userOne = new User();
 $userTwo = new User();
 
-echo 'class of $userOne is ' . get_class($userOne);
+echo $userOne->username . '<br />';
+echo $userOne->email . '<br />';
+echo $userOne->addFriend() . '<br />';
+
+// print_r ( get_class_vars('User') );
+// print_r ( get_class_methods('User') );
+
+$userTwo->username = 'mimir';
+$userTwo->email = 'mimir@odin.com';
+
+
+echo $userTwo->username . '<br />';
+echo $userTwo->email . '<br />';
+echo $userTwo->addFriend() . '<br />';
+
+
 
 ?>
 
